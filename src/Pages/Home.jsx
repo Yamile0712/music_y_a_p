33bf7f8a13f7c47b9580161970a5692d6a8cd958
Yamile0712 <yamileariasperez07@gmail.com>
2026-03-09 11:31:42 -0500

    inicio

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,14 @@
+export const Home = () => {
+    return (
+
+        <>
+            <div className="container mt-5">
+                <p>Solo Para Valientes</p>
+                <h1>BIENBENIDOS A LA ZONA <br /> DE TERROR</h1>
+                <img src="" alt="" />
+            </div>
+
+        </>
+
+    )
+}
---
+++ src/components/Header.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export const Header = () => {
+  return (
+    <>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary border-bottom border-success">
+      <div className="container">
+        <a className="navbar-brand" href="#">MUSIC</a>
+       
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+
+          <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+            <li className="nav-item dropdown">
+               <a className="nav-link" href="#">Home</a>
+            </li>
+
+            
+            <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+              <li className="nav-item dropdown">
+               <a className="nav-link" href="#">Favoritas</a>
+              </li>
+            </ul>
+
+            <li className="nav-item">
+           <a className="nav-link" href="#">Careers</a>
+            </li>
+
+            <li className="nav-item">
+            <a className="nav-link " aria-disabled="true">About</a>
+            
+            </li>
+          </ul>
+          <div>
+    
+            
+            <button type="button" className="btn btn-outline-secondary border border-success">Search</button>
+            
+          </div>
+        </div>
+      </div>
+    </nav>
+    </>
+  )
+}
